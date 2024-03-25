be1922a26c2cc71e8c26ca74b1ee5a2e01924e85
Duncan Rout <duncanrout@yahoo.com>
2024-03-25 14:14:54 -0500

    added fade in

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -6,16 +7,25 @@ import Aboutmy from './components/Aboutmy';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import { colors } from './palette';
+import FadeInText from './components/FadeInText'; // Import the FadeInText component
 
 function App() {
   return (
-    // temp fix for white line ate bottom
     <div className="App" style={{ backgroundColor: colors.forthText }}>
       <Navbar />
-      <Whoami />
-      <Aboutmy />
-      <Projects />
-      <Contact />
+      {/* Use FadeInText component for elements you want to fade in */}
+      <FadeInText>
+        <Whoami />
+      </FadeInText>
+      <FadeInText>
+        <Aboutmy />
+      </FadeInText>
+      <FadeInText>
+        <Projects />
+      </FadeInText>
+      <FadeInText>
+        <Contact />
+      </FadeInText>
       <Footer />
     </div>
   );
---
+++ src/components/FadeInText.jsx
@@ -0,0 +1,45 @@
+// Import necessary dependencies
+import React, { useRef, useEffect, useState } from 'react';
+import './FadeInText.css'; // Create a new CSS file for styling
+
+// Define a functional component for fade-in text
+const FadeInText = ({ children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const fadeInRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5 // Adjust this threshold as needed
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(entry.target); // Stop observing once the element is visible
+        }
+      });
+    }, options);
+
+    if (fadeInRef.current) {
+      observer.observe(fadeInRef.current);
+    }
+
+    return () => {
+      if (fadeInRef.current) {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        observer.unobserve(fadeInRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <div ref={fadeInRef} className={`fade-in-text ${isVisible ? 'visible' : ''}`}>
+      {children}
+    </div>
+  );
+};
+
+export default FadeInText;
